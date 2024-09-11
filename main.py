f18e2db447a2cc05985e2a018b9da8c3d7ffc1e6
kathlyn-sts <kathlyn.santos1@pucpr.edu.br>
2024-09-11 11:12:08 -0300

    Update01

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 127.0.0.1.8000/teste
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# 127.0.0.1.8000/teste1
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste" : "deu certo"}
